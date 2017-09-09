18dbf3d0e1d83a447072b00d56ab2fbd29cd7e57
Himanshu Dagar <hdagar3@gmail.com>
2017-09-10 02:26:51 +0530

    Data Preprocessing and Regression files

--- Regression/Polynomial-Regression/Polynomial_Regression/polynomial_regression.py
+++ Regression/Polynomial-Regression/Polynomial_Regression/polynomial_regression.py
@@ -29,7 +29,6 @@ lin_reg.fit(X, y)
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
-poly_reg.fit(X_poly, y)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
